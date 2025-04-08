90565ccf95b606924faab4425ba71af4420611ad
capellinx <capellaaa7@gmail.com>
2025-04-08 01:06:07 -0300

    feat: add auth provider

--- src/app/root/providers.tsx
+++ src/app/root/providers.tsx
@@ -1,14 +1,17 @@
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthBackOfficeProvider } from '@/context/auth-backoffice-context';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <main>
-        {children}
-      </main>
+      <AuthBackOfficeProvider>
+        <main>
+          {children}
+        </main>
+      </AuthBackOfficeProvider>
     </QueryClientProvider>
   );
 }
