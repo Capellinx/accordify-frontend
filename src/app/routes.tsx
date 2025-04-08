a68d1926f8f004b51db19f70fdc4ed92ef1e89d5
capellinx <capellaaa7@gmail.com>
2025-04-08 01:29:31 -0300

    feat: create protected route backoffice

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,21 +1,31 @@
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
-import { App } from './root';
+import { Root } from './root';
 import { Login } from '@/pages/backoffice/login';
 import { AuthLayout } from '@/layouts/auth-layout';
 import { Paths } from '@/shared/paths';
+import { Dashboard } from '@/pages/backoffice/dashboard';
+import { AuthBackofficeProtected } from './auth-protected/auth-backoffice-protected';
 
 
 const routes: RouteObject[] = [{
     path: '/',
-    element: <App />,
-    children: [
-        { children: [{
-            element: <AuthLayout />,
-            children: [
-                { path: Paths.backoffice.login, Component: Login }
-            ]
-        }]}
-    ],
+    element: <Root />,
+    children: [{
+        children: [
+            {
+                element: <AuthLayout />,
+                children: [
+                    { path: Paths.backoffice.login, Component: Login },
+                ]
+            },
+            {children: [{
+                element: <AuthBackofficeProtected/>,
+                children: [
+                    { path: Paths.backoffice.dashboard, Component: Dashboard }
+                ]
+            }]},
+        ],
+    }],
 }];
 
 
