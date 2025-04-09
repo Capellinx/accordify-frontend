31ec7dbf76fec25d3f3f40b820459a2448df9610
capellinx <capellaaa7@gmail.com>
2025-04-08 23:49:39 -0300

    refactor: change validation to use acess_token =

--- src/app/auth-protected/auth-backoffice-protected.tsx
+++ src/app/auth-protected/auth-backoffice-protected.tsx
@@ -1,12 +1,12 @@
-import { useAuthBackOffice } from "@/hooks/useAuthBackoffice"
+import { LOCALSTORAGE } from "@/shared/local-storage-keys"
 import { Paths } from "@/shared/paths"
 import { Navigate, Outlet } from "react-router-dom"
 
 
 export function AuthBackofficeProtected() {
-   const { manager } = useAuthBackOffice()
+   const access_token = localStorage.getItem(LOCALSTORAGE.BACKOFFICE.LOGIN)
 
-   if(!manager){
+   if (!access_token){
       return Navigate({
          to: Paths.backoffice.login,
          replace: true
