a6bc35c7383c9922e955397850b5a5cc07528ee0
capellinx <capellaaa7@gmail.com>
2025-04-09 20:36:39 -0300

    feat: define toaster provider

--- src/app/root/providers.tsx
+++ src/app/root/providers.tsx
@@ -1,17 +1,19 @@
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthBackOfficeProvider } from '@/context/auth-backoffice-context';
+import { Toaster } from "@/components/ui/sonner"
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-        <AuthBackOfficeProvider>
-            <main>
-              {children}
-            </main>
-        </AuthBackOfficeProvider>
+      <AuthBackOfficeProvider>
+        <main>
+          {children}
+        </main>
+        <Toaster />
+      </AuthBackOfficeProvider>
     </QueryClientProvider>
   );
 }
