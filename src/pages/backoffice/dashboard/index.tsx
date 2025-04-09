268d0ba57f8fe5bddf988db751e6b1432b2f4e04
capellinx <capellaaa7@gmail.com>
2025-04-08 23:34:42 -0300

    feat: create dashboard

--- src/pages/backoffice/dashboard/index.tsx
+++ src/pages/backoffice/dashboard/index.tsx
@@ -1,7 +1,64 @@
+import * as React from "react"
+import {
+   Frame,
+   Map,
+   PieChart,
+} from "lucide-react"
 
+import { NavProjects } from "@/pages/backoffice/dashboard/components/nav-projects"
+import { NavUser } from "@/pages/backoffice/dashboard/components/nav-user"
+import { TeamSwitcher } from "@/pages/backoffice/dashboard/components/team-switcher"
+import {
+   Sidebar,
+   SidebarContent,
+   SidebarFooter,
+   SidebarHeader,
+   SidebarRail,
+} from "@/components/ui/sidebar"
 
-export function Dashboard() {
+const data = {
+   user: {
+      name: "shadcn",
+      email: "m@example.com",
+      avatar: "/avatars/shadcn.jpg",
+   },
+   projects: [
+      {
+         name: "Clientes",
+         url: "#",
+         icon: Frame,
+      },
+      {
+         name: "Times",
+         url: "#",
+         icon: PieChart,
+      },
+      {
+         name: "Usuários",
+         url: "#",
+         icon: Map,
+      },
+      {
+         name: "Assinaturas",
+         url: "#",
+         icon: Map,
+      },
+   ],
+}
+
+export function Dashboard({ ...props }: React.ComponentProps<typeof Sidebar>) {
    return (
-      <h1>Sou um dashboard</h1>
+      <Sidebar collapsible="icon" {...props}>
+         <SidebarHeader>
+            <TeamSwitcher />
+         </SidebarHeader>
+         <SidebarContent>
+            <NavProjects projects={data.projects} />
+         </SidebarContent>
+         <SidebarFooter>
+            <NavUser user={data.user} />
+         </SidebarFooter>
+         <SidebarRail />
+      </Sidebar>
    )
 }
