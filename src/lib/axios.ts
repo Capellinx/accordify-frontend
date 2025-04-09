f04856af0f8a00bacd3ac3b60c83d797c481f8be
capellinx <capellaaa7@gmail.com>
2025-04-09 01:19:26 -0300

    feat: define folder lib to render librarys

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// todo: implementar middleware de captura de error
 export const api = axios.create({
-   baseURL: 'http://localhost:3333', 
+   baseURL: 'http://localhost:3333',
    headers: {
       'Content-Type': 'application/json',
    },
