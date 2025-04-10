b13018ae2b0e2c053885f1f105f9ec398a8845b2
capellinx <capellaaa7@gmail.com>
2025-04-09 21:01:59 -0300

    refactor: update backoffice layout

--- src/layouts/backoffice-layout.tsx
+++ src/layouts/backoffice-layout.tsx
@@ -1,13 +1,17 @@
-import { ThemeProvider } from "@/pages/backoffice/dashboard/components/theme-provider";
+import { SidebarLayout } from "@/components/sidebar";
+import { ThemeProvider } from "@/components/sidebar/components/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 
 
 export function BackOfficeLayout() {
    return (
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
          <SidebarProvider>
-            <Outlet />
+            <div className="flex gap-10 p-20">
+               <SidebarLayout />
+               <Outlet />
+            </div>
          </SidebarProvider>
       </ThemeProvider>
    )
