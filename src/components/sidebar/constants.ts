dffe180164f9dae0c41dca9ee1a1e7542d548248
capellinx <capellaaa7@gmail.com>
2025-04-09 21:02:13 -0300

    feat: create constant sidebar control

---
+++ src/components/sidebar/constants.ts
@@ -0,0 +1,32 @@
+import { Paths } from "@/shared/paths";
+import { Frame, PieChart, Map } from "lucide-react";
+
+export const SIDEBAR_CONTROL = {
+   user: {
+      name: "shadcn",
+      email: "m@example.com",
+      avatar: "/avatars/shadcn.jpg",
+   },
+   projects: [
+      {
+         name: "Clientes",
+         url: Paths.backoffice.clients,
+         icon: Frame,
+      },
+      {
+         name: "Times",
+         url: "#",
+         icon: PieChart,
+      },
+      {
+         name: "Usuários",
+         url: "#",
+         icon: Map,
+      },
+      {
+         name: "Assinaturas",
+         url: "#",
+         icon: Map,
+      },
+   ],
+}
