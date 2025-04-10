d9c4b92ae478b9437918ef13e0d9f324fc0ae916
capellinx <capellaaa7@gmail.com>
2025-04-09 21:01:34 -0300

    feat: add new route

--- src/shared/paths.ts
+++ src/shared/paths.ts
@@ -2,6 +2,7 @@ export const Paths = {
    backoffice: {
       login: "/backoffice/login",
       dashboard: "/backoffice/dashboard",
+      clients: "/backoffice/clients",
       resetPassword: "/backoffice/reset-password"
    }
 }
